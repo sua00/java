f02c41ff67c20ac1ef92736eb47489dfc65ddbd4
sua00 <88168765+sua00@users.noreply.github.com>
2022-10-03 17:48:56 +0900

    Add files via upload

---
+++ charpractice.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class charpractice {
+public static void main(String[] args){
+    String a = "Hello World";
+    System.out.println(a.charAt(0)); //H
+
+    String b = "3456";
+    int c = b.charAt(3)-'0';
+    System.out.println(c);  //6
+}
+ }
+
+
